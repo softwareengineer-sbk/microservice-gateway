3b5928723ed9cd52ca695b36e314f00b7e4a82d4
Sardorbek Khalimboev <khalimboevsardorbek@gmail.com>
2022-08-09 15:21:02 +0500

    initial setup

---
+++ microservice-gateway/src/main/java/self/training/app/Application.java
@@ -0,0 +1,39 @@
+package self.training.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class Application {
+    public static void main( String[] args ) {
+        SpringApplication.run(Application.class, args);
+    }
+
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route(p -> p
+//                        .path("/companyName/teacher/**")
+//                        .filters(f -> f.rewritePath("/companyName/teacher/(?<segment>.*)", "/${segment}")
+//                                .addResponseHeader("X-Response-Time", new Date().toString()))
+//                        .uri("lb://microservice-teacher"))
+//                .route(p -> p
+//                        .path("/companyName/student/**")
+//                        .filters(f -> f.rewritePath("/companyName/student/(?<segment>.*)", "/${segment}")
+//                                .addResponseHeader("X-Response-Time", new Date().toString()))
+//                        .uri("lb://microservice-student"))
+//                .route(p -> p
+//                        .path("/companyName/payment/**")
+//                        .filters(f -> f.rewritePath("/companyName/payment/(?<segment>.*)", "/${segment}")
+//                                .addResponseHeader("X-Response-Time", new Date().toString()))
+//                        .uri("lb://microservice-payment"))
+//                .build();
+//    }
+}
