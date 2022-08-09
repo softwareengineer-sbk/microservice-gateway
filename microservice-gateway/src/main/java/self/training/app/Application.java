cb70a4c81cd63a665dfcd3e7a2f1fa8fea11e23f
Sardorbek Khalimboev <khalimboevsardorbek@gmail.com>
2022-08-09 15:21:16 +0500

    initial setup 2

--- microservice-gateway/src/main/java/self/training/app/Application.java
+++ microservice-gateway/src/main/java/self/training/app/Application.java
@@ -2,12 +2,7 @@ package self.training.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Date;
 
 @EnableEurekaClient
 @SpringBootApplication
